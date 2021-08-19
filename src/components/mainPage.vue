4a0c4e12a46cadeb85dd1efe830fa7278245cba5
asmi046 <asmi046@gmail.com>
2021-08-19 16:38:44 +0300

    Интерфейс менеджера

--- src/components/mainPage.vue
+++ src/components/mainPage.vue
@@ -1,12 +1,29 @@
 <template>
-  <div class="pageWrapper">
-     <h1>Привет</h1> 
+  <div>
+      <v-app-bar> 
+        <v-app-bar-nav-icon @click="showMenu = !showMenu"></v-app-bar-nav-icon>
+        <v-toolbar-title>Панель инструментов менеджера</v-toolbar-title>
+      </v-app-bar>
+      
+      <v-navigation-drawer
+        v-model="showMenu"
+        absolute
+        temporary
+      ></v-navigation-drawer>
+
+      <v-content>
+          <h1>Привет</h1>
+      </v-content>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {  
+      showMenu:false
+    }
+  }
 }
 </script>
 
