fb9adeb864f8143c59be6c472a6247aa345a85c2
asmi046 <asmi046@gmail.com>
2021-08-25 10:15:52 +0300

    Таблица заказа

--- src/components/addZakazForm.vue
+++ src/components/addZakazForm.vue
@@ -6,30 +6,61 @@
             </v-col>
         </v-row>
         <v-form>
-            <v-row >
-                <v-col cols="6" sm = "12">
+            <v-row>
+                <v-col md = "6" cols = "12">
+                    <v-text-field label="Номер заказа" prepend-inner-icon="mdi-tag" ></v-text-field>
+                </v-col>
+                
+                <v-col md = "6" cols = "12">
+                    <v-text-field label="Дата оформления" prepend-inner-icon="mdi-calendar" ></v-text-field>
+                </v-col>
+            </v-row>
+
+            <v-row>
+                <v-col md = "6" cols = "12" >
                     <v-text-field label="Клиент" prepend-inner-icon="mdi-account" ></v-text-field>
                 </v-col>
-                <v-col cols="3" sm = "12">
+                <v-col md = "3"  cols="12">
                     <v-text-field v-model="phone" v-mask="'+# (###) ###-##-##'" label="Телефон" prepend-inner-icon="mdi-card-account-phone" ></v-text-field>
                 </v-col>
-                <v-col cols="3" sm = "12">
+                <v-col  md = "3" cols="12">
                     <v-text-field  v-model="phone2" v-mask="'+# (###) ###-##-##'" label="Телефон" prepend-inner-icon="mdi-card-account-phone-outline" ></v-text-field>
                 </v-col>
             </v-row>
                 
             <v-row>
-                <v-col>
+                <v-col >
                     <v-text-field label="Адрес доставки" prepend-inner-icon="mdi-map-marker-multiple" ></v-text-field>
                 </v-col>
             </v-row>
 
             <v-row>
-                <v-col>
+                <v-col md = "6" cols = "12">
                     <v-text-field label="Номер счета" prepend-inner-icon="mdi-file-document" ></v-text-field>
                 </v-col>
+                <v-col md = "6" cols = "12">
+                    <v-text-field  label="Сумма счета" prepend-inner-icon="mdi-currency-rub" ></v-text-field>
+                </v-col>
+            </v-row>
+
+            <v-row>
                 <v-col>
-                    <v-text-field label="Сумма счета" prepend-inner-icon="mdi-currency-rub" ></v-text-field>
+                    <v-data-table
+                        dense
+                        :headers="headers"
+                        :items="desserts"
+                        item-key="name"
+                        class="elevation-1"
+                        hideDefaultFooter = "false"
+                    ></v-data-table>                    
+                </v-col>
+            </v-row>
+            
+            <v-row>
+                <v-col>
+                    <v-btn color="secondary">
+                        <v-icon>mdi-plus</v-icon> Добавить товар
+                    </v-btn>
                 </v-col>
             </v-row>
         </v-form>
@@ -41,7 +72,35 @@ export default {
     data() {
         return {
             phone:"",
-            phone2:""
+            phone2:"",
+
+            headers: [
+                {text: "Наименование", value: "name"},
+                {text: "Количество", value: "count"},
+                {text: "Цена", value: "price"},
+                {text: "Сумма", value: "summ"}
+            ],
+
+             desserts: [ 
+                 {
+                    name: "Лампа №1",
+                    count: 1,
+                    price: 500,
+                    summ: 500
+                 },
+                 {
+                    name: "Лампа №2",
+                    count: 3,
+                    price: 1000,
+                    summ: 3000
+                 },
+                 {
+                    name: "Лампа №3",
+                    count: 3,
+                    price: 5000,
+                    summ: 15000
+                 }
+             ]
         }
     }
 }
